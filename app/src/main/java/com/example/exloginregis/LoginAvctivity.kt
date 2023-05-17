f6d17eec4e80522053a9db56fe97faf1f4635535
TLink174 <linh174ht@gmail.com>
2023-05-17 14:39:24 +0700

    xong

--- app/src/main/java/com/example/exloginregis/LoginAvctivity.kt
+++ app/src/main/java/com/example/exloginregis/LoginAvctivity.kt
@@ -2,6 +2,7 @@ package com.example.exloginregis
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
@@ -10,34 +11,47 @@ import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_regiz.*
 import kotlinx.android.synthetic.main.activity_regiz.sign_in_ek6
 import kotlinx.android.synthetic.main.activity_regiz.sign_up_ek1
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginAvctivity : AppCompatActivity() {
-    private lateinit var auth: DatabaseReference
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        auth = FirebaseDatabase.getInstance().getReference("Users")
-        sign_in_ek6.setOnClickListener {
-            SigninUser()
-        }
-        sign_up.setOnClickListener {
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+//        auth = FirebaseAuth.getInstance()
+//        sign_in_ek6.setOnClickListener {
+//            SigninUser()
+//        }
+        val btnSignUp = findViewById<Button>(R.id.sign_up)
+
+        btnSignUp.setOnClickListener {
             val intent = Intent(this, RegisActivity::class.java)
             startActivity(intent)
+        }
+
+        val btnSignIn = findViewById<Button>(R.id.sign_in_ek6)
+        btnSignIn.setOnClickListener {
+            SigninUser()
         }
     }
 
     private fun SigninUser(){
         val email = enter_your_email.text.toString()
         val pass = password.text.toString()
-        if (email.isEmpty()){
-            enter_your_email.error = "Email is required"
-            return
-        }
-        if (pass.isEmpty()){
-            password.error = "Password is required"
+        if (email.isEmpty() || pass.isEmpty()) {
+            Toast.makeText(this, "Please enter your email and password", Toast.LENGTH_SHORT).show()
             return
         }
         auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) { task ->
+            // Tạo đối tượng User
             if (task.isSuccessful) {
                 // Đăng nhập thành công, chuyển đến màn hình chính
                 startActivity(Intent(this, MainActivity::class.java))
@@ -46,9 +60,12 @@ class LoginAvctivity : AppCompatActivity() {
                 // Đăng nhập thất bại, hiển thị thông báo lỗi
                 val exception = task.exception
                 val errorMessage = exception?.message
-                Toast.makeText(baseContext, "Authentication failed: $errorMessage", Toast.LENGTH_SHORT).show()
-        }
-
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed: $errorMessage",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
--- app/src/main/java/com/example/exloginregis/RegisActivity.kt
+++ app/src/main/java/com/example/exloginregis/RegisActivity.kt
@@ -2,20 +2,31 @@ package com.example.exloginregis
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_regiz.*
 
-class RegisActivity: AppCompatActivity() {
-    private lateinit var db: DatabaseReference
+class RegisActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_regiz)
 
-    db = FirebaseDatabase.getInstance().getReference("Users")
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
+        val btnRegister = findViewById<Button>(R.id.sign_in_ek6)
+
+        btnRegister.setOnClickListener {
+            SingupUsersdata()
+        }
         sign_up_ek1.setOnClickListener {
             SingupUsersdata()
         }
@@ -24,28 +35,37 @@ class RegisActivity: AppCompatActivity() {
     private fun SingupUsersdata() {
         val email = enter_your_email_ek1.text.toString()
         val pass = password_ek1.text.toString()
+
+        if (email.isEmpty() || pass.isEmpty()) {
+            Toast.makeText(this, "Please enter your email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
         //day du lieu
-        val iduser = db.push().key!!
-        val user = UserModel(iduser, email, pass)
-        //check
-        if (email.isEmpty()) {
-            enter_your_email_ek1.error = "Email is required"
-            return
-        }
-        if (pass.isEmpty()) {
-            password_ek1.error = "Password is required"
-            return
-        }
-        db.child(iduser).setValue(user)
-            .addOnCanceledListener {
-                Toast.makeText(this, "Regis thanh cong", Toast.LENGTH_SHORT).show()
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-                enter_your_email_ek1.setText("")
-                password_ek1.setText("")
+
+                Toast.makeText(
+                    baseContext,
+                    "Authentication success.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                // If sign in fails, display a message to the user.
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed.",
+                    Toast.LENGTH_SHORT,
+                ).show()
             }
-            .addOnFailureListener { err ->
-                Toast.makeText(this, "${err.message}", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed. ${it.localizedMessage}",
+                    Toast.LENGTH_SHORT,
+                ).show()
             }
     }
 }
